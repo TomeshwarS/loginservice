bbdbeb71781630d7adb1f4855d5a4775595b78be
Tomeshwar Shiwane <Tomeshwar Shiwane@IB-Pune-LAP-052>
2020-05-02 01:02:26 +0530

    provided application name & changes uri.

--- src/main/java/com/infobeans/loginservice/controller/UsersManagerController.java
+++ src/main/java/com/infobeans/loginservice/controller/UsersManagerController.java
@@ -1,14 +1,11 @@
 package com.infobeans.loginservice.controller;
 
-import javax.ws.rs.Produces;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.infobeans.loginservice.model.User;
